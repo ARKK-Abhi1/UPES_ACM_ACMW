8b76e39e73291bd54ff3ea7c94c279b587b7ff67
azycray <500060253@stu.upes.ac.in>
2018-06-04 19:08:56 +0530

    Changes made in Bottom Navigation, Left Navigation, Post layout

--- app/src/main/java/org/upesacm/acmacmw/SplashActivity.java
+++ app/src/main/java/org/upesacm/acmacmw/SplashActivity.java
@@ -5,6 +5,8 @@ import  android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.upesacm.acmacmw.activity.HomeActivity;
+
 public class SplashActivity extends AppCompatActivity {
       private static int SPLASH_TIME_OUT=2000;
     @Override
@@ -14,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashActivity.this,EventRegistration.class);
+                Intent homeIntent = new Intent(SplashActivity.this,HomeActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
