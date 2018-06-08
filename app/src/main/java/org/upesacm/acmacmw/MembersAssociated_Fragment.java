1cf93842c59b9e7f0f1bbe5cb0f60d1f10cfced5
akkuratra <500060879@stu.upes.ac.in>
2018-06-08 16:19:00 +0530

    changes

--- app/src/main/java/org/upesacm/acmacmw/MembersAssociated_Fragment.java
+++ app/src/main/java/org/upesacm/acmacmw/MembersAssociated_Fragment.java
@@ -1,13 +1,19 @@
 package org.upesacm.acmacmw;
 
+import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MembersAssociated_Fragment extends AppCompatActivity {
-
+public class MembersAssociated_Fragment extends Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_members_associated__fragment);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_ongoing_projects, container,false);
+
     }
+
 }
